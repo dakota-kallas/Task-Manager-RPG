9242746473c73bc97d1706a31919d9cb72b9432f
Dakota Kallas <79180508+dakota-kallas@users.noreply.github.com>
2021-04-22 16:50:45 -0500

    Update task_manager.py

--- task_manager.py
+++ task_manager.py
@@ -1,7 +1,7 @@
-# Creating a window with different tabs that can be used
+# A task manager combined with an Role-Playing Game.
 #
-# CS 224 Team Project
-# Date: 4/9/21
+# Authors: Dakota Kallas, Chase Roehl, Cailyn Paul
+# Date: 4/22/21
 
 import os
 from os import path
@@ -10,6 +10,8 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 from tkinter import *
+import requests
+import json
 
 # global list for storing all of the tasks
 task_dict = {'Hunger':0, 'Hygiene':1, 'Strength':2, 'Energy':3, 'Mood':4, 'Thirst':5}
@@ -41,6 +43,17 @@ def input_error() :
    
   return 1
 
+def name_error() :
+  # check for enter task field is empty or not
+  if name_field.get() == "":
+       
+      # show the error message
+      messagebox.showerror("Invalid Name Error")
+       
+      return 0
+   
+  return 1
+
 # Return the type of task. (If not found return -1)
 def find_type(cur_task):
   if cur_task in tasks_list[0]:
@@ -58,27 +71,48 @@ def find_type(cur_task):
   else:
     return -1
 
+# Check to ensure a valid user input
+def check_user_input(input):
+  global counter
+
+  # handling the empty task error
+  if counter == 1 :
+    messagebox.showerror("Error", "No tasks")
+    return
+
+  # checking for input error when
+  # empty input in task number field
+  if input == "\n" :
+    messagebox.showerror("Error", "Input error")
+    return False
+
+  try:
+    # Convert it into integer
+    input = int(input)
+  except ValueError:
+    messagebox.showerror("Error", "Input error")
+    return False
+
+  # checking for input error when
+  # input is greater than total tasks
+  if input >= counter :
+    messagebox.showerror("Error", "Input too high")
+    return False
+
+  return True
+
 # Function used to mark a task complete
 def complete_task():
   global counter
   global completed
   global xp
 
-  # handling the empty task error
-  if counter == 1 :
-    messagebox.showerror("No tasks")
-    return
-  
   # get the task number, which is required to delete
   number = task_number_field.get(1.0, 'end')
   
-  # checking for input error when
-  # empty input in task number field
-  if number == "\n" :
-    messagebox.showerror("Input error")
+  if check_user_input(number) == False:
     return
-  else :
-    task_no = int(number)
+  task_no = int(number)
   
   # Deleting the content of task number field
   clear_task_number_field()
@@ -156,21 +190,12 @@ def char_select():
 def delete_task():
   global counter
 
-  # handling the empty task error
-  if counter == 1 :
-    messagebox.showerror("No tasks")
-    return
-  
   # get the task number, which is required to delete
   number = task_number_field.get(1.0, 'end')
   
-  # checking for input error when
-  # empty input in task number field
-  if number == "\n" :
-    messagebox.showerror("Input error")
+  if check_user_input(number) == False:
     return
-  else :
-    task_no = int(number)
+  task_no = int(number)
   
   # Deleting the content of task number field
   clear_task_number_field()
@@ -196,6 +221,23 @@ def delete_task():
   for i in range(len(all_tasks)) :
     text_area.insert('end -1 chars', "[ " + str(i + 1) + " ] " + all_tasks[i] + " ( " + find_type(all_tasks[i]) + " )\n")
 
+# Function for generating a random task
+def insert_random_task():
+  global counter
+  # Gets the task from the api 
+  response = requests.get('https://www.boredapi.com/api/activity/')
+  decode_response = json.loads(response.content.decode('utf-8'))
+  task = decode_response.get('activity')
+
+  # Add task to the mood task list
+  tasks_list[4].append(task)
+  all_tasks.append(task)
+
+  # Insert it into the text area 
+  text_area.insert('end -1 chars', "[ " + str(counter) + " ] " + task + " ( Mood )\n")
+
+  # add 1 to the counter
+  counter += 1
 
 # Function used to insert tasks
 def insert_task():
@@ -381,22 +423,22 @@ if __name__ == "__main__" :
   
   # Create the window & instantiate its size
   win = tk.Tk()
-  win.title("Tasky Boi")
-  win.geometry("900x650")
+  win.title("Task Manager by DK, CR, and CP")
+  win.geometry("900x680")
 
   style = ttk.Style()
 
   light = '#ffb997'
-  dark = '#f67e7d'
+  dark = '#4d3620'
   
-  style.theme_create( "yummy", parent="alt", settings={
+  style.theme_create( "tasky", parent="alt", settings={
         "TNotebook": {"configure": {"tabmargins": [2, 5, 2, 0] } },
         "TNotebook.Tab": {
             "configure": {"padding": [5, 1], "background": light },
             "map":       {"background": [("selected", dark)],
                           "expand": [("selected", [2, 2, 2, 0])] } } } )
 
-  style.theme_use("yummy")
+  style.theme_use("tasky")
 
   tab_parent = ttk.Notebook(win)
 
@@ -435,8 +477,15 @@ if __name__ == "__main__" :
   cur_img = os.path.join(img_dir, 'selecttask.png')
   select_task_label = PhotoImage(file = cur_img)
   task_number = tk.Label(tab_tasks, bg = 'grey', image = select_task_label)
+
+  # create a button : Tasks Completed
+  cur_img = os.path.join(img_dir, 'taskscompleted.png')
+  task_completed_label = PhotoImage(file = cur_img)
+  tasks_completed = tk.Label(tab_tasks, bg = 'grey', image = task_completed_label)
   
   # create a button label : Submit
+  # when user press the button, the command or 
+  # function affiliated to that button is executed 
   cur_img = os.path.join(img_dir, 'submit.png')
   submit_label = PhotoImage(file = cur_img)
   submit = tk.Button(tab_tasks, bg = 'grey', command = insert_task, image = submit_label)
@@ -452,17 +501,12 @@ if __name__ == "__main__" :
   complete = tk.Button(tab_tasks, bg = 'grey', command = complete_task, image = mark_complete_label)
  
   # create a button label : Save
-  cur_img = os.path.join(img_dir, 'delete.png')
+  cur_img = os.path.join(img_dir, 'save.png')
   save_label = PhotoImage(file = cur_img)
   save_button = tk.Button(tab_tasks, bg = 'grey', command = save, image = save_label)
  
-  # create a text entry box 
-  # for typing the task
+  # create a text entry box for typing the task
   enter_task_field = tk.Entry(tab_tasks, font=("Trebuchet", 15))
-
-  # create a Submit Button and place into the root window
-  # when user press the button, the command or 
-  # function affiliated to that button is executed 
   
   # datatype of menu text 
   clicked = StringVar() 
@@ -481,22 +525,26 @@ if __name__ == "__main__" :
   # text area is for writing the content
   text_area = tk.Text(tab_tasks, height = 20, width = 25, font = "lucida 18")
 
-  # create a label : Select Task Number
-                        
+  # create a label : Select Task Number                      
   task_number_field = tk.Text(tab_tasks, height = 1, width = 3, font = "lucida 18")
   
   # Create a counter for the tasks completed
-  tasks_completed = tk.Label(tab_tasks, text = "Tasks Completed", bg = med_light, font=("Trebuchet", 15))
   total_tasks_completed = StringVar()
   amount_tasks = tk.Label(tab_tasks, textvariable=total_tasks_completed, font=("Trebuchet", 15))
 
   total_tasks_completed.set(completed)
+
+  # Create a button: Bored?
+  cur_img = os.path.join(img_dir, 'bored.png')
+  bored_label = PhotoImage(file = cur_img)
+  bored_button = tk.Button(tab_tasks, bg = 'grey', command = insert_random_task, image = bored_label)
 
   # Create a gird of the tasks tab
   enter_task.grid(row=0, column=1, padx=15, pady=8)
   enter_task_field.grid(row=1, column=1, padx=15, pady=3)
   submit.grid(row=3, column=1, padx=15, pady=3)
   drop.grid(row=2, column=1, padx=15, pady=3)
+  bored_button.grid(row=28, column=1, padx=15, pady=8)
 
   my_tasks.grid(row=0, column=2, padx=15, pady=8)
   text_area.grid(row=1, column=2, padx=15, pady=3, rowspan=45)
@@ -512,13 +560,17 @@ if __name__ == "__main__" :
   ##### Statistics Tab #####
 
   # create a label : Xp Level
-  xp_label = tk.Label(tab_stats, text = "XP Level", bg = med_light, font=("Trebuchet", 30))
+  cur_img = os.path.join(img_dir, 'xplevel.png')
+  xp_level_img = PhotoImage(file = cur_img)
+  xp_label = tk.Label(tab_stats, bg = dark, image = xp_level_img)
   cur_xp_level = StringVar()
   xp_level_label = tk.Label(tab_stats, textvariable=cur_xp_level, font=("Trebuchet", 20))
 
   cur_xp_level.set(xp_level)
 
-  xp_tot_label = tk.Label(tab_stats, text = "Progress", bg = med_light, font=("Trebuchet", 30))
+  cur_img = os.path.join(img_dir, 'progress.png')
+  progress_img = PhotoImage(file = cur_img)
+  xp_tot_label = tk.Label(tab_stats, bg = dark, image = progress_img)
   cur_tot_xp = StringVar()
   xp_prog_label = tk.Label(tab_stats, textvariable=cur_tot_xp, font=("Trebuchet", 20))
 
@@ -543,14 +595,30 @@ if __name__ == "__main__" :
   character = PhotoImage( file = cur_img) 
   character_label = tk.Label(tab_stats, bg = 'grey', image = character)
 
-
   ## Create a label for each of the task types ##
-  hunger_label = tk.Label(tab_stats, text = "Hunger", bg = med_dark, font = ("Trebuchet, 30"))
-  thirst_label = tk.Label(tab_stats, text = "Thirst", bg = med_dark, font = ("Trebuchet, 30"))
-  energy_label = tk.Label(tab_stats, text = "Energy", bg = med_dark, font = ("Trebuchet, 30"))
-  strength_label = tk.Label(tab_stats, text = "Strength", bg = med_dark, font = ("Trebuchet, 30"))
-  hygiene_label = tk.Label(tab_stats, text = "Hygiene", bg = med_dark, font = ("Trebuchet, 30"))
-  mood_label = tk.Label(tab_stats, text = "Mood", bg = med_dark, font = ("Trebuchet, 30"))
+  cur_img = os.path.join(img_dir, 'hunger.png')
+  hunger_img = PhotoImage(file = cur_img)
+  hunger_label = tk.Label(tab_stats, bg = dark, image = hunger_img)
+
+  cur_img = os.path.join(img_dir, 'thirst.png')
+  thirst_img = PhotoImage(file = cur_img)
+  thirst_label = tk.Label(tab_stats, bg = dark, image = thirst_img)
+
+  cur_img = os.path.join(img_dir, 'energy.png')
+  energy_img = PhotoImage(file = cur_img)
+  energy_label = tk.Label(tab_stats, bg = dark, image = energy_img)
+
+  cur_img = os.path.join(img_dir, 'strength.png')
+  strength_img = PhotoImage(file = cur_img)
+  strength_label = tk.Label(tab_stats, bg = dark, image = strength_img)
+
+  cur_img = os.path.join(img_dir, 'hygiene.png')
+  hygiene_img = PhotoImage(file = cur_img)
+  hygiene_label = tk.Label(tab_stats, bg = dark, image = hygiene_img)
+
+  cur_img = os.path.join(img_dir, 'mood.png')
+  mood_img = PhotoImage(file = cur_img)
+  mood_label = tk.Label(tab_stats, bg = dark, image = mood_img)
  
   hunger_xp = StringVar()
   hunger_xp_label = tk.Label(tab_stats, textvariable=hunger_xp, font=("Trebuchet", 15))
